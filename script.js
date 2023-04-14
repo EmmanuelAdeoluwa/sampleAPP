bcbe4be45aa1f2c435a4cb8e70280ab8138ce9b0
EmmanuelAdeoluwa <emmanueladeoluwa16@gmail.com>
2023-04-14 07:56:47 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//Git is just where you put your coding projects especiallly when you are working with a team 
+//and you need to track changes of team members
+//Github is a website to host your repositories online where you host your projects online
+
+//Repository: simply just means projects/place where your projects is kept
+
+
+//Git Commands
+//all the commands used in Git are in lowercase
+// 1) Clone: This brings a repository that is hosted somewhere like Github into a folder on your local machine
+// 2) add: This tracks your files and changes in Git
+// 3) commit: This is used to save your files in Git
+// 4) push: Upload git commits(i.e projects wey you don save) to a remote repo, like Github
+// 5) pulL; This downloads changes from remote repo to your local machine. opposite of push
+
+//$git innit initialize local git repo
+//git add<file>
+// git status - check status of working tree
+//git commit - commit changes in index
+//git add 
+//git rm
+//git push - will push to remote repo(github) SSH keys can be done with
+//git add. will add all your files
+//git pull - will pull from remote repo
+//git clone - clone repo into a new directory
+//git-scm.com/download/win for installing
+
+
+//You can initialize your git in your vscode terminal. git innit
+//then add your username and password
+// git config --global user.name "EmmanuelAdeoluwa"
+// git config --global user.email "emmanueladeoluwa16@gmail.com"
